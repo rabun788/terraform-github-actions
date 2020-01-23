a410730694e5497d2a488b840802d1b9aef7bd65
kxj3808 <kyle_jackson@homedepot.com>
2020-01-23 15:20:30 -0500

    removing exit code

--- src/terraform_init.sh
+++ src/terraform_init.sh
@@ -15,7 +15,7 @@ function terraformInit {
 
    echo "${initOutput}"
    wait
-   initExitCode=${?}
+#   initExitCode=${?}
 
 
    # Exit code of 0 indicates success. Print the output and exit.
@@ -49,6 +49,6 @@ function terraformInit {
     echo "${initPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${initCommentsURL}" > /dev/null
   fi
 
-#  exit ${initExitCode}
+  exit ${initExitCode}
 }
 
